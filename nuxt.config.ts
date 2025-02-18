ec4a13d6010a4dda3e25d964204d04c35cb4f8b0
thisisal1ev <aaalievvv1@gmail.com>
2025-02-18 08:18:02 +0500

    add initial layout and Tailwind CSS integration

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -4,5 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  vite: { plugins: [tailwindcss()] }
+  vite: { plugins: [tailwindcss()] },
+  css: ['./assets/index.css'],
 })

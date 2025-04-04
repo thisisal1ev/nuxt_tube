fd557c55f9df109d8f62d7a3241fc4f096b59d70
thisisal1ev <aaalievvv1@gmail.com>
2025-04-04 13:57:59 +0500

    feat: integrate Tailwind CSS and additional modules
    - Updated nuxt.config.ts to include Tailwind CSS, @nuxt/image, @nuxt/fonts, and @nuxt/icon modules.
    - Added custom font configuration for Nunito and SVG icon settings.
    - Created index.css to define global styles using Tailwind CSS.
    - Added a simple index.vue page with a "Hello world!" message.
    - Updated package.json to include new dependencies for Tailwind CSS and Nuxt modules.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,22 @@
+import tailwindcss from '@tailwindcss/vite'
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+	compatibilityDate: '2024-11-01',
+	devtools: { enabled: true },
+	css: ['~/assets/index.css'],
+	vite: { plugins: [tailwindcss()] },
+	modules: ['@nuxt/image', '@nuxt/fonts', '@nuxt/icon'],
+
+	fonts: {
+		families: [{ name: 'Nunito', provider: 'google', global: true }],
+		experimental: {
+			processCSSVariables: true,
+		},
+	},
+
+	icon: {
+		mode: 'svg',
+		size: '16px',
+	},
 })

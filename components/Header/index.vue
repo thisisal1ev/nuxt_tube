c4c8222e536903eadab5bfca0d9fb0708aa6e681
thisisal1ev <aaalievvv1@gmail.com>
2025-04-04 18:52:41 +0500

    feat: implement Header and Sidebar components with layout styling

--- components/Header/index.vue
+++ components/Header/index.vue
@@ -1,3 +1,47 @@
 <template>
-	Header
+	<header class='header'>
+		<div class='header_container'>
+			<input type="search" placeholder="Search..." class="search_input transition_color" />
+
+			<div class="space-x-5 flex items-center justify-between">
+				<button>
+					<Icon name="lucide:square-plus" class="icon" />
+				</button>
+
+				<NuxtLink to="/studio">
+					<Icon name="lucide:layout-grid" class="icon" />
+				</NuxtLink>
+
+				<button>
+					<Icon name="lucide:bell" class="icon" />
+				</button>
+
+				<NuxtLink to="/auth" class="auth_button transition_color bg-red hover:bg-red/80">
+					<Icon name="lucide:log-in" />
+
+					<span>Auth</span>
+				</NuxtLink>
+			</div>
+		</div>
+	</header>
 </template>
+
+<style scoped>
+@reference "tailwindcss";
+
+.header {
+	@apply border-b border-white/10 px-5 py-3;
+}
+
+.header_container {
+	@apply flex items-center justify-center space-x-10;
+}
+
+.search_input {
+	@apply grow rounded text-sm px-4 py-2 outline-none hover:bg-white/10 focus:bg-transparent placeholder:text-sm
+}
+
+.auth_button {
+	@apply text-white rounded-sm px-4 py-1.5 flex items-center space-x-1.5
+}
+</style>
--- components/Sidebar/index.vue
+++ components/Sidebar/index.vue
@@ -1,3 +1,13 @@
 <template>
-	Sidebar
+	<aside class='sidebar'>
+		asdsad
+	</aside>
 </template>
+
+<style scoped>
+@reference "tailwindcss";
+
+.sidebar {
+	@apply border-r border-white/10 min-h-screen space-y-5 outline-none p-5 w-60;
+}
+</style>

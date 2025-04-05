1badab14844a4f491eb7176ddcb1daf8f0001b4c
thisisal1ev <aaalievvv1@gmail.com>
2025-04-05 20:15:41 +0500

    feat: implement Card component for improved video display and reusability

---
+++ components/Card/index.vue
@@ -0,0 +1,53 @@
+<script lang='ts' setup>
+interface Props {
+	id: number
+	poster: string
+	avatar: string
+	title: string
+	views: string
+	channel: string
+	createdAt: string
+	isOfficial: boolean
+	isTrend: boolean
+}
+
+defineProps<Props>()
+</script>
+
+<template>
+	<li class='w-full flex flex-col'>
+		<NuxtLink :to="`/video/${id}`" class='relative'>
+			<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow" :src="poster" :alt="title" />
+
+			<NuxtLink :to='`/channel/${channel}`' class='absolute bottom-4 left-2.5'>
+				<NuxtImg width='40' height='40' :src="avatar" :alt='channel' class='rounded-full' />
+			</NuxtLink>
+		</NuxtLink>
+
+		<div class="space-y-1.5 grow">
+			<div class="flex items-center justify-between">
+				<div class="flex items-center space-x-0.5">
+					<Icon v-if='isTrend' name="lucide:flame" class="text-dimmed-red" size='16' />
+
+					<span class="text-white/50 text-xs tabular-nums">
+						{{ views }} views
+					</span>
+				</div>
+
+				<p class="text-white/50 text-xs">{{ createdAt }}</p>
+			</div>
+
+			<h4 class="font-semibold text-sm overflow-hidden line-clamp-2 group">
+				<NuxtLink :to='`/video/${id}`' class="transition-colors duration-300 group-hover:text-white/70">
+					{{ title }}
+				</NuxtLink>
+			</h4>
+
+			<NuxtLink :to="`/channel/${channel}`" class="flex items-center text-sm">
+				<span class="mr-1.5 text-white/70 text-sm">{{ channel }}</span>
+
+				<Icon v-if='isOfficial' name="lucide:badge-check" class="text-green-600" />
+			</NuxtLink>
+		</div>
+	</li>
+</template>
--- pages/(root)/index.vue
+++ pages/(root)/index.vue
@@ -3,149 +3,28 @@
 		<SectionTitle icon='flame' text='Trending' />
 
 		<ul role='list' class='wrapper'>
-			<li class='w-full flex flex-col'>
-				<NuxtLink :to="`/video/1`" class='relative'>
-					<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow"
-						src="https://i.ytimg.com/vi/7eaURiSE_s8/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLDPjrX7AiyZWEtlqiqbOCz4-UWKNg"
-						alt="title" />
+			<Card :key='1' :id='1'
+				poster='https://i.ytimg.com/vi/7eaURiSE_s8/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLDPjrX7AiyZWEtlqiqbOCz4-UWKNg'
+				avatar='https://yt3.googleusercontent.com/q_M_xI1c2K2osn5jwuJ69rbYA4qJCpN_thaojC5V-dlNhlEiGBbMsjuh9vHvvKLkEPSI6KYF=s160-c-k-c0x00ffffff-no-rj'
+				channel='Macan' title='MACAN, A.V.G - Привыкаю' views='1.5k' createdAt='2 days ago' isTrend isOfficial />
 
-					<NuxtImg width='40' height='40'
-						src="https://yt3.googleusercontent.com/q_M_xI1c2K2osn5jwuJ69rbYA4qJCpN_thaojC5V-dlNhlEiGBbMsjuh9vHvvKLkEPSI6KYF=s160-c-k-c0x00ffffff-no-rj"
-						alt='Avatar' class='absolute rounded-full bottom-4 left-2.5' />
-				</NuxtLink>
+			<Card :key='2' :id='2'
+				poster='https://i.ytimg.com/vi/7dnvtlYi0Vc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCdzT0JHTKBuBAqXj6uvuBTHT1UbQ'
+				avatar='https://yt3.googleusercontent.com/BHcM28mKcOC3ZTUzwCPqZlU7nWfK0iqlOuRUH448NqxNtKAtSU9LYohWgxbKSojlUoKF5kMMvQ=s160-c-k-c0x00ffffff-no-rj'
+				title='Зачем тебе Switch 2, если ты первый не прошёл?' channel='Rozetked' views='2k' createdAt='2 days ago'
+				isTrend isOfficial />
 
-				<div class="space-y-1.5 grow">
-					<div class="flex items-center justify-between">
-						<div class="flex items-center space-x-0.5">
-							<Icon name="lucide:flame" class="text-dimmed-red" size='16' />
+			<Card :key='3' :id='3'
+				poster='https://i9.ytimg.com/vi/Eiu9hzJ-YrE/hq720_custom_2.jpg?sqp=CKzexL8G-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCIJw9Oo9QKyHGaMXpI00cVv1KrdA'
+				avatar='https://yt3.ggpht.com/aiw73zdQg8OFJXVUK0m4rFJnxSUn5GArkXrLWD3sDh8LJ0eAVpdn9ECmXSKyHyyoX98pbKYVWQ=s88-c-k-c0x00ffffff-no-rj'
+				channel='RED Group' title='Я ПРЕДУПРЕЖДАЛ! УЯЗВИМОСТЬ в NEXT JS' views='1M' createdAt='a month ago' isTrend
+				isOfficial />
 
-							<span class="text-white/50 text-xs tabular-nums">
-								1.5k views
-							</span>
-						</div>
-
-						<p class="text-white/50 text-xs">2 days ago</p>
-					</div>
-
-					<h4 class="font-semibold text-sm overflow-hidden line-clamp-2">
-						MACAN, A.V.G - Привыкаю
-					</h4>
-
-					<NuxtLink :to="`/channel/macan`" class="flex items-center text-sm">
-						<span class="mr-1.5 text-white/70 text-sm">Macan</span>
-
-						<Icon name="lucide:badge-check" class="text-green-600" />
-					</NuxtLink>
-				</div>
-			</li>
-
-			<li class='w-full flex flex-col'>
-				<NuxtLink :to="`/video/1`" class='relative'>
-					<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow"
-						src="https://i.ytimg.com/vi/7dnvtlYi0Vc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCdzT0JHTKBuBAqXj6uvuBTHT1UbQ"
-						alt="title" />
-
-					<NuxtImg width='40' height='40'
-						src="https://yt3.googleusercontent.com/BHcM28mKcOC3ZTUzwCPqZlU7nWfK0iqlOuRUH448NqxNtKAtSU9LYohWgxbKSojlUoKF5kMMvQ=s160-c-k-c0x00ffffff-no-rj"
-						alt='Avatar' class='absolute rounded-full bottom-4 left-2.5' />
-				</NuxtLink>
-
-				<div class="space-y-1.5 grow">
-					<div class="flex items-center justify-between">
-						<div class="flex items-center space-x-0.5">
-							<Icon name="lucide:flame" class="text-dimmed-red" size='16' />
-
-							<span class="text-white/50 text-xs tabular-nums">
-								1.5k views
-							</span>
-						</div>
-
-						<p class="text-white/50 text-xs">2 days ago</p>
-					</div>
-
-					<h4 class="font-semibold text-sm overflow-hidden line-clamp-2">
-						Зачем тебе Switch 2, если ты первый не прошёл?
-					</h4>
-
-					<NuxtLink :to="`/channel/macan`" class="flex items-center text-sm">
-						<span class="mr-1.5 text-white/70 text-sm">Rozetked</span>
-
-						<Icon name="lucide:badge-check" class="text-green-600" />
-					</NuxtLink>
-				</div>
-			</li>
-
-			<li class='w-full flex flex-col'>
-				<NuxtLink :to="`/video/1`" class='relative'>
-					<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow"
-						src="https://i9.ytimg.com/vi/Eiu9hzJ-YrE/hq720_custom_2.jpg?sqp=CKzexL8G-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCIJw9Oo9QKyHGaMXpI00cVv1KrdA"
-						alt="title" />
-
-					<NuxtImg width='40' height='40'
-						src="https://yt3.ggpht.com/aiw73zdQg8OFJXVUK0m4rFJnxSUn5GArkXrLWD3sDh8LJ0eAVpdn9ECmXSKyHyyoX98pbKYVWQ=s88-c-k-c0x00ffffff-no-rj"
-						alt='Avatar' class='absolute rounded-full bottom-4 left-2.5' />
-				</NuxtLink>
-
-				<div class="space-y-1.5 grow">
-					<div class="flex items-center justify-between">
-						<div class="flex items-center space-x-0.5">
-							<Icon name="lucide:flame" class="text-dimmed-red" size='16' />
-
-							<span class="text-white/50 text-xs tabular-nums">
-								1.5k views
-							</span>
-						</div>
-
-						<p class="text-white/50 text-xs">2 days ago</p>
-					</div>
-
-					<h4 class="font-semibold text-sm overflow-hidden line-clamp-2">
-						Я ПРЕДУПРЕЖДАЛ! УЯЗВИМОСТЬ в NEXT JS
-					</h4>
-
-					<NuxtLink :to="`/channel/macan`" class="flex items-center text-sm">
-						<span class="mr-1.5 text-white/70 text-sm">RED Group</span>
-
-						<Icon name="lucide:badge-check" class="text-green-600" />
-					</NuxtLink>
-				</div>
-			</li>
-
-			<li class='w-full flex flex-col'>
-				<NuxtLink :to="`/video/1`" class='relative'>
-					<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow"
-						src="https://i.ytimg.com/vi/cPYTxu0vTOk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwzMTm4uhVqGJGmeJyo7NDRnX90w"
-						alt="title" />
-
-					<NuxtImg width='40' height='40'
-						src="https://yt3.googleusercontent.com/8Ux80QiA5C5NgasL0mETDJUYVZObERuoQykwCiviSVFgr1q05AuvFwX5yoXUNtotLE8kLkQsqA=s160-c-k-c0x00ffffff-no-rj"
-						alt='Avatar' class='absolute rounded-full bottom-4 left-2.5' />
-				</NuxtLink>
-
-				<div class="space-y-1.5 grow">
-					<div class="flex items-center justify-between">
-						<div class="flex items-center space-x-0.5">
-							<Icon name="lucide:flame" class="text-dimmed-red" size='16' />
-
-							<span class="text-white/50 text-xs tabular-nums">
-								1.5k views
-							</span>
-						</div>
-
-						<p class="text-white/50 text-xs">2 days ago</p>
-					</div>
-
-					<h4 class="font-semibold text-sm overflow-hidden line-clamp-2">
-						Курс Nuxt 3. Урок 1. Знакомство с фреймворком
-					</h4>
-
-					<NuxtLink :to="`/channel/macan`" class="flex items-center text-sm">
-						<span class="mr-1.5 text-white/70 text-sm">CodeHorizon</span>
-
-						<Icon name="lucide:badge-check" class="text-green-600" />
-					</NuxtLink>
-				</div>
-			</li>
+			<Card :key='4' :id='4'
+				poster='https://i.ytimg.com/vi/cPYTxu0vTOk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwzMTm4uhVqGJGmeJyo7NDRnX90w'
+				avatar='https://yt3.googleusercontent.com/8Ux80QiA5C5NgasL0mETDJUYVZObERuoQykwCiviSVFgr1q05AuvFwX5yoXUNtotLE8kLkQsqA=s160-c-k-c0x00ffffff-no-rj'
+				title='Курс Nuxt 3. Урок 1. Знакомство с фреймворком' channel='CodeHorizon' views='1.5k' createdAt='2 days ago'
+				isTrend :isOfficial='false' />
 		</ul>
 	</section>
 
@@ -153,165 +32,35 @@
 		<SectionTitle icon='compass' text='Explore' />
 
 		<ul role='list' class='wrapper'>
-			<li class='w-full flex flex-col'>
-				<NuxtLink :to="`/video/1`" class='relative'>
-					<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow"
-						src="https://i.ytimg.com/vi/7eaURiSE_s8/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLDPjrX7AiyZWEtlqiqbOCz4-UWKNg"
-						alt="title" />
+			<Card :key='1' :id='1'
+				poster='https://i.ytimg.com/vi/7eaURiSE_s8/hqdefault.jpg?sqp=-oaymwE2CNACELwBSFXyq4qpAygIARUAAIhCGAFwAcABBvABAfgB_gmAAtAFigIMCAAQARhlIGUoZTAP&rs=AOn4CLDPjrX7AiyZWEtlqiqbOCz4-UWKNg'
+				avatar='https://yt3.googleusercontent.com/q_M_xI1c2K2osn5jwuJ69rbYA4qJCpN_thaojC5V-dlNhlEiGBbMsjuh9vHvvKLkEPSI6KYF=s160-c-k-c0x00ffffff-no-rj'
+				channel='Macan' title='MACAN, A.V.G - Привыкаю' views='1.5k' createdAt='2 days ago' :isTrend='false'
+				isOfficial />
 
-					<NuxtImg width='40' height='40'
-						src="https://yt3.googleusercontent.com/q_M_xI1c2K2osn5jwuJ69rbYA4qJCpN_thaojC5V-dlNhlEiGBbMsjuh9vHvvKLkEPSI6KYF=s160-c-k-c0x00ffffff-no-rj"
-						alt='Avatar' class='absolute rounded-full bottom-4 left-2.5' />
-				</NuxtLink>
+			<Card :key='2' :id='2'
+				poster='https://i.ytimg.com/vi/7dnvtlYi0Vc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCdzT0JHTKBuBAqXj6uvuBTHT1UbQ'
+				avatar='https://yt3.googleusercontent.com/BHcM28mKcOC3ZTUzwCPqZlU7nWfK0iqlOuRUH448NqxNtKAtSU9LYohWgxbKSojlUoKF5kMMvQ=s160-c-k-c0x00ffffff-no-rj'
+				title='Зачем тебе Switch 2, если ты первый не прошёл?' channel='Rozetked' views='2k' createdAt='2 days ago'
+				:isTrend='false' isOfficial />
 
-				<div class="space-y-1.5 grow">
-					<div class="flex items-center justify-between">
-						<span class="text-white/50 text-xs tabular-nums">
-							1.5k views
-						</span>
+			<Card :key='3' :id='3'
+				poster='https://i9.ytimg.com/vi/Eiu9hzJ-YrE/hq720_custom_2.jpg?sqp=CKzexL8G-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCIJw9Oo9QKyHGaMXpI00cVv1KrdA'
+				avatar='https://yt3.ggpht.com/aiw73zdQg8OFJXVUK0m4rFJnxSUn5GArkXrLWD3sDh8LJ0eAVpdn9ECmXSKyHyyoX98pbKYVWQ=s88-c-k-c0x00ffffff-no-rj'
+				channel='RED Group' title='Я ПРЕДУПРЕЖДАЛ! УЯЗВИМОСТЬ в NEXT JS' views='1M' createdAt='a month ago'
+				:isTrend='false' isOfficial />
 
-						<p class="text-white/50 text-xs">2 days ago</p>
-					</div>
+			<Card :key='4' :id='4'
+				poster='https://i.ytimg.com/vi/cPYTxu0vTOk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwzMTm4uhVqGJGmeJyo7NDRnX90w'
+				avatar='https://yt3.googleusercontent.com/8Ux80QiA5C5NgasL0mETDJUYVZObERuoQykwCiviSVFgr1q05AuvFwX5yoXUNtotLE8kLkQsqA=s160-c-k-c0x00ffffff-no-rj'
+				title='Курс Nuxt 3. Урок 1. Знакомство с фреймворком' channel='CodeHorizon' views='1.5k' createdAt='2 days ago'
+				:isTrend='false' :isOfficial='false' />
 
-					<h4 class="font-semibold text-sm overflow-hidden line-clamp-2">
-						MACAN, A.V.G - Привыкаю
-					</h4>
-
-					<NuxtLink :to="`/channel/macan`" class="flex items-center text-sm">
-						<span class="mr-1.5 text-white/70 text-sm">Macan</span>
-
-						<Icon name="lucide:badge-check" class="text-green-600" />
-					</NuxtLink>
-				</div>
-			</li>
-
-			<li class='w-full flex flex-col'>
-				<NuxtLink :to="`/video/1`" class='relative'>
-					<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow"
-						src="https://i.ytimg.com/vi/7dnvtlYi0Vc/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCdzT0JHTKBuBAqXj6uvuBTHT1UbQ"
-						alt="title" />
-
-					<NuxtImg width='40' height='40'
-						src="https://yt3.googleusercontent.com/BHcM28mKcOC3ZTUzwCPqZlU7nWfK0iqlOuRUH448NqxNtKAtSU9LYohWgxbKSojlUoKF5kMMvQ=s160-c-k-c0x00ffffff-no-rj"
-						alt='Avatar' class='absolute rounded-full bottom-4 left-2.5' />
-				</NuxtLink>
-
-				<div class="space-y-1.5 grow">
-					<div class="flex items-center justify-between">
-						<span class="text-white/50 text-xs tabular-nums">
-							1.5k views
-						</span>
-
-						<p class="text-white/50 text-xs">2 days ago</p>
-					</div>
-
-					<h4 class="font-semibold text-sm overflow-hidden line-clamp-2">
-						Зачем тебе Switch 2, если ты первый не прошёл?
-					</h4>
-
-					<NuxtLink :to="`/channel/macan`" class="flex items-center text-sm">
-						<span class="mr-1.5 text-white/70 text-sm">Rozetked</span>
-
-						<Icon name="lucide:badge-check" class="text-green-600" />
-					</NuxtLink>
-				</div>
-			</li>
-
-			<li class='w-full flex flex-col'>
-				<NuxtLink :to="`/video/1`" class='relative'>
-					<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow"
-						src="https://i9.ytimg.com/vi/Eiu9hzJ-YrE/hq720_custom_2.jpg?sqp=CKzexL8G-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCIJw9Oo9QKyHGaMXpI00cVv1KrdA"
-						alt="title" />
-
-					<NuxtImg width='40' height='40'
-						src="https://yt3.ggpht.com/aiw73zdQg8OFJXVUK0m4rFJnxSUn5GArkXrLWD3sDh8LJ0eAVpdn9ECmXSKyHyyoX98pbKYVWQ=s88-c-k-c0x00ffffff-no-rj"
-						alt='Avatar' class='absolute rounded-full bottom-4 left-2.5' />
-				</NuxtLink>
-
-				<div class="space-y-1.5 grow">
-					<div class="flex items-center justify-between">
-						<span class="text-white/50 text-xs tabular-nums">
-							1.5k views
-						</span>
-
-						<p class="text-white/50 text-xs">2 days ago</p>
-					</div>
-
-					<h4 class="font-semibold text-sm overflow-hidden line-clamp-2">
-						Я ПРЕДУПРЕЖДАЛ! УЯЗВИМОСТЬ в NEXT JS
-					</h4>
-
-					<NuxtLink :to="`/channel/macan`" class="flex items-center text-sm">
-						<span class="mr-1.5 text-white/70 text-sm">RED Group</span>
-
-						<Icon name="lucide:badge-check" class="text-green-600" />
-					</NuxtLink>
-				</div>
-			</li>
-
-			<li class='w-full flex flex-col'>
-				<NuxtLink :to="`/video/1`" class='relative'>
-					<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow"
-						src="https://i.ytimg.com/vi/cPYTxu0vTOk/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBwzMTm4uhVqGJGmeJyo7NDRnX90w"
-						alt="title" />
-
-					<NuxtImg width='40' height='40'
-						src="https://yt3.googleusercontent.com/8Ux80QiA5C5NgasL0mETDJUYVZObERuoQykwCiviSVFgr1q05AuvFwX5yoXUNtotLE8kLkQsqA=s160-c-k-c0x00ffffff-no-rj"
-						alt='Avatar' class='absolute rounded-full bottom-4 left-2.5' />
-				</NuxtLink>
-
-				<div class="space-y-1.5 grow">
-					<div class="flex items-center justify-between">
-						<span class="text-white/50 text-xs tabular-nums">
-							1.5k views
-						</span>
-
-						<p class="text-white/50 text-xs">2 days ago</p>
-					</div>
-
-					<h4 class="font-semibold text-sm overflow-hidden line-clamp-2">
-						Курс Nuxt 3. Урок 1. Знакомство с фреймворком
-					</h4>
-
-					<NuxtLink :to="`/channel/macan`" class="flex items-center text-sm">
-						<span class="mr-1.5 text-white/70 text-sm">CodeHorizon</span>
-
-						<Icon name="lucide:badge-check" class="text-green-600" />
-					</NuxtLink>
-				</div>
-			</li>
-
-			<li class='w-full flex flex-col'>
-				<NuxtLink :to="`/video/1`" class='relative'>
-					<NuxtImg width="300" height="150" class="w-full rounded-md mb-2 aspect-video grow"
-						src="https://i.ytimg.com/vi/_Pv6wfABwF0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDHZTJ4MYr9t05CTTIQZePej-GKjQ"
-						alt="title" />
-
-					<NuxtImg width='40' height='40'
-						src="https://yt3.ggpht.com/aiw73zdQg8OFJXVUK0m4rFJnxSUn5GArkXrLWD3sDh8LJ0eAVpdn9ECmXSKyHyyoX98pbKYVWQ=s88-c-k-c0x00ffffff-no-rj"
-						alt='Avatar' class='absolute rounded-full bottom-4 left-2.5' />
-				</NuxtLink>
-
-				<div class="space-y-1.5 grow">
-					<div class="flex items-center justify-between">
-						<span class="text-white/50 text-xs tabular-nums">
-							1.5k views
-						</span>
-
-						<p class="text-white/50 text-xs">2 days ago</p>
-					</div>
-
-					<h4 class="font-semibold text-sm overflow-hidden line-clamp-2">
-						10 ОШИБОК, которые ЛОМАЮТ ваши ПРОЕКТЫ!
-					</h4>
-
-					<NuxtLink :to="`/channel/macan`" class="flex items-center text-sm">
-						<span class="mr-1.5 text-white/70 text-sm">RED Group</span>
-
-						<Icon name="lucide:badge-check" class="text-green-600" />
-					</NuxtLink>
-				</div>
-			</li>
+			<Card :key='5' :id='5'
+				poster='https://i.ytimg.com/vi/_Pv6wfABwF0/hqdefault.jpg?sqp=-oaymwEcCNACELwBSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLDHZTJ4MYr9t05CTTIQZePej-GKjQ'
+				avatar='https://yt3.ggpht.com/aiw73zdQg8OFJXVUK0m4rFJnxSUn5GArkXrLWD3sDh8LJ0eAVpdn9ECmXSKyHyyoX98pbKYVWQ=s88-c-k-c0x00ffffff-no-rj'
+				channel='RED Group' title='10 ОШИБОК, которые ЛОМАЮТ ваши ПРОЕКТЫ!' views='65.2k' created-at='3 month ago'
+				:isTrend='false' isOfficial />
 		</ul>
 	</section>
 </template>
